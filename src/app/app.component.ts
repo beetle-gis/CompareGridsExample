a1b27294a1a926a699313ac27a4eeb99b0fdd323
Petro.Zhuk Symphony <p.zhuk@gis-art.com>
2023-03-12 17:52:25 +0200

    second commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,16 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
 import {
-  DataBindingDirective,
+  DataBindingDirective, DataStateChangeEvent,
 } from '@progress/kendo-angular-grid';
 import {
   process,
+  State,
+  aggregateBy,
+  AggregateResult,
+  AggregateDescriptor,
 } from "@progress/kendo-data-query";
 import { trips } from './trips';
+import {FormBuilder, FormGroup} from "@angular/forms";
 
 @Component({
   selector: 'my-app',
@@ -17,11 +22,24 @@ export class AppComponent implements OnInit {
   public gridData: unknown[] = trips;
   public gridView: unknown[];
 
+  public formGroup: FormGroup;
+
   public mySelection: string[] = [];
+
+  public total: any;
+
+  constructor(private formBuilder: FormBuilder) {
+    this.createFormGroup = this.createFormGroup.bind(this);
+  }
 
   public ngOnInit(): void {
     this.gridView = this.gridData;
+    this.total = aggregateBy(this.gridView, this.aggregates);
   }
+
+  public aggregates: AggregateDescriptor[] = [
+    { field: "billing.cost", aggregate: "sum" },
+  ];
 
   public onFilter(input: Event): void {
     const inputValue = (input.target as HTMLInputElement).value;
@@ -61,4 +79,23 @@ export class AppComponent implements OnInit {
 
     this.dataBinding.skip = 0;
   }
+
+  public dataStateChange(state: DataStateChangeEvent): void {
+    console.log(state);
+    // this.state = state;
+    // this.gridData = process(sampleProducts, this.state);
+    // this.total = aggregateBy(this.gridData.data, this.aggregates);
+  }
+
+  public createFormGroup = (args: any): FormGroup => {
+    const item = args.isNew ? {} : args.dataItem;
+
+    this.formGroup = this.formBuilder.group({
+      // 'status.name': [item['status.name']],
+      trip_id: [item.trip_id],
+      /*...*/
+    });
+
+    return this.formGroup;
+  };
 }
